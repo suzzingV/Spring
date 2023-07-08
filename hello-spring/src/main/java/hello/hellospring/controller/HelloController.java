1815b98f42472153493ed7443249e25e1415a8e5
suzzingv <sjlim1999@naver.com>
2023-07-08 23:10:07 +0900

    spring web 개발

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,36 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); //data가 hello!!로 치환
         return "hello"; //templetes의 hello.html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-templete";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body부에 return 내용 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //hello sujin
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
