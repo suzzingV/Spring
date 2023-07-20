fbd12d3cd09c18e8ad1ab109b01f35865deb917c
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-07-20 23:35:32 +0900

    탐색 위치와 기본 스캔대상

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(
+@ComponentScan( //basepackage 지정 안하면 이 클래스의 package인 hello.core에서 뒤짐
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //AppConfig 제외
 )
 public class AutoAppConfig {
--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //안에 ComponentScan있음 -> 스프링부트 쓰면 자동으로 됨
 public class CoreApplication {
 
 	public static void main(String[] args) {
