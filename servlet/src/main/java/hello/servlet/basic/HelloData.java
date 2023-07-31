29b72d211c54286730f25dc37e112049e7c3f1e7
suzzingv <sjlim1999@naver.com>
2023-08-01 02:54:08 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,11 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter //lombok 다운하면 가능
+public class HelloData {
+
+    private String username;
+    private int age;
+}
