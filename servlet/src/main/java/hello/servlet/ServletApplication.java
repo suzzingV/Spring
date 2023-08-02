b1a5551507729e493146c8489deaf2208b197f4c
suzzingv <sjlim1999@naver.com>
2023-08-03 04:36:10 +0900

    뷰 리졸버

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //내 패키지부터 하위 패키지 다 뒤져서 서블릿 찾아서 등록하고 실행하게 도움
 @SpringBootApplication
