f82608ddf58f1aefb4afc1137331961a4cbcfe6a
suzzingv <sjlim1999@naver.com>
2023-08-02 22:21:37 +0900

    단순하고 실용적인 컨트롤러 - v4

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -2,8 +2,6 @@ package hello.servlet.web.frontcontroller.v4.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
 
 import java.util.List;
@@ -17,6 +15,6 @@ public class MemberListControllerV4 implements ControllerV4 {
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
         List<Member> members = memberRepository.findAll();
         model.put("members", members);
-        return "";
+        return "members";
     }
 }
