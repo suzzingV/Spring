e9acd6dd5627147c8f46a468ae7025c55e30dea8
suzzingv <sjlim1999@naver.com>
2023-08-02 17:47:57 +0900

    Model 추가 - v3

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -17,5 +18,16 @@ public class MyView {
     public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
         dispatcher.forward(req, resp);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        modelToRequestAttribute(model, req);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach((key, value) -> req.setAttribute(key, value));
     }
 }
