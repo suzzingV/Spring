0c6e28c44b1c3b547d7d1a8460fc3594a4d53490
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-08-05 14:43:06 +0900

    HTTP 요청 파라미터-쿼리 파라미터, HTML Form

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParameterController.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParameterController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        resp.getWriter().write("ok");
+    }
+}
