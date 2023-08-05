74a3df29400a648c0b9a1a9d8eca2b0930b872e3
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-08-05 15:12:09 +0900

    HTTP  요청 파라미터-@RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParameterController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParameterController.java
@@ -3,10 +3,13 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,61 @@ public class RequestParameterController {
         log.info("username={}, age={}", username, age);
 
         resp.getWriter().write("ok");
+    }
+
+    @ResponseBody //ok를 그대로 http 메시지에 넣음 뷰조회 안하고
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) { //int에 null들어갈수 없어서 오류 -> Integer로 바꿔줘야
+//null, ""; 다름
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault( //default값 있으면 required 의미 없음
+            @RequestParam(defaultValue = "guest") String username, //defaultValue가 있어서 필수값인데도 안넣어도 오류 안뜸
+            @RequestParam(defaultValue = "-1") int age) { //int에 null들어갈수 없어서 오류 -> Integer로 바꿔줘야
+//null, ""; 다름
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
