e7972c0fa92931107ab8cfab52cf1a6e3de62fd6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-08-18 14:15:12 +0900

    JDBC 개발 - 수정, 삭제

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -93,6 +93,47 @@ public class MemberRepositoryV0 {
             {
                 close(con, pstmt, rs);
             }
-        }ㄴ
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(2, memberId);
+            pstmt.setInt(1, money);
+            pstmt.executeUpdate();
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {} ", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
     }
 }
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +19,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("member4", 10000);
+        Member member = new Member("member7", 10000);
         repositoryV0.save(member);
 
         //findById
@@ -27,5 +28,15 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}" , member.equals(findMember)); //lombok의 @Data -> equals, hashcode 만들어줌 -> 내용물 같으면 같게 취급
         assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repositoryV0.update(member.getMemberId(), 20000);
+        Member updateMember = repositoryV0.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repositoryV0.delete(member.getMemberId());
+        assertThatThrownBy(() -> repositoryV0.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
