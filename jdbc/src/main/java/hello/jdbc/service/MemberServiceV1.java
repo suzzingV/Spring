388df33c860fc19d7b0101c93a4a6a5e3623b579
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-08-19 20:24:08 +0900

    트랜잭션-적용1

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor //의존성 주입의 방법 중 생성자 주입을 임의의 코드 없이 자동으로 설정
+
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+
+    }
+
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
